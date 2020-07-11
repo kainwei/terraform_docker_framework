62842e1ef8ee5a1fd0c061fc2367058ea255651e
kainvey@gmail.com <kainvey@gmail.com>
2020-07-12 01:20:22 +1200

    update

--- credential.tf
+++ credential.tf
@@ -1,8 +1,10 @@
 variable "access_key" {
+  # Add your access_key value here and be careful, don't upload to public place!!!
   default = ""
 }
 
 variable "secret_key" {
+  # Add your secret_key value here  and be careful, don't upload to public place!!!
   default = ""
 }
 
