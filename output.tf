8e53014fa33e8b7c747af44ac8dcdfe3017e9ae4
kainvey@gmail.com <kainvey@gmail.com>
2020-07-12 14:47:34 +1200

    update

--- output.tf
+++ output.tf
@@ -1,14 +1,16 @@
-output "EC2_IP:" {
+output "EC2_IP" {
   value=module.ec2.ip
 }
-output "EC2_LOG_DIR:" {
-  value=module.ec2.ip + "/log/"
+
+output "EC2_LOG_PATH" {
+  value="http://${module.ec2.ip}/log/"
 }
 
-output "ELB_DNS_NAME:" {
+output "ELB_DNS_NAME" {
   value=module.elb.dns_name
 }
 
-output "ELB_LOG_DIR:" {
-  value=module.elb.dns_name + "/log/"
+output "ELB_LOG_PATH" {
+  value="http://${module.elb.dns_name}/log/"
 }
+
